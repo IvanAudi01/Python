ae9379ea6c55a6a56389feebfec7ac1acc8a8893
Iván Audiffred <84604820+IvanAudi01@users.noreply.github.com>
2023-08-16 07:41:32 -0600

    Add files via upload

---
+++ 00_helloWorld.py
@@ -0,0 +1,12 @@
+#Hola mundo
+#Iván Alfonso Hernández Audiffred
+print(" ")
+print("Hello world!!!")
+
+ #Conocer tipo de cadenas
+print(type("Hola amigo 'Me siento genial' ")) #Tipo srt
+print(type(1))        #type int
+print(type(1.4))      #type float
+print(type(True))     #type bool
+print(type(1 + 3j))   #type complex
+print(" ")
